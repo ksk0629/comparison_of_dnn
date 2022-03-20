cd883f6c0a0736571543cf601eb98a5450ff990d
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2022-03-20 05:34:54 +0000

    Refacored codes

--- src/california_dnn.py
+++ src/california_dnn.py
@@ -1,9 +1,9 @@
 import argparse
-import yaml
-import mlflow
 
-from tensorflow import keras
+import mlflow
 import pandas as pd
+from tensorflow import keras
+import yaml
 
 from california_dataset import CaliforniaDataset
 from custom_dnn import CustomDNN
@@ -30,49 +30,44 @@ class CaliforniaDNN(CustomDNN):
         return keras.losses.MeanSquaredError(reduction="auto", name="mean_squared_error")
 
     @staticmethod
-    def run_all_process_with_mlflow(config_yaml_path: str):
-        """
-        Build, train and evaluate the DNN, whose the structure is specified by config file, with mlflow.
+    def run_all_process_with_mlflow(config_yaml_path: str) -> object:
+        """Build, train and evaluate the DNN, whose the structure is specified by config file, with mlflow.
 
-        Parameters
-        ----------
-        config_yaml_path : str
-            the config about the model structure, training, and experiment information
-
-        Return
-        ------
-        california_dnn : CaliforniaDNN
-            trained CaliforniaDNN
+        :param str config_yaml_path: the config about the model structure, training, and experiment information
+        :return obejct: trained CaliforniaDNN
         """
-        # Load config
+        # Load configs
         with open(config_yaml_path, "r") as yaml_f:
             config = yaml.safe_load(yaml_f)
         config_mlflow = config["mlflow"]
         config_dataset = config["dataset"]
         dnn_parameters = config["dnn"]
         dnn_train_parameters = config["dnn_train"]
-        
+
+        # Start training and evaluating with mlflow
         mlflow.set_experiment(config_mlflow["experiment_name"])
-        with mlflow.start_run(run_name=config_mlflow["run_name"]) as run:
+        with mlflow.start_run(run_name=config_mlflow["run_name"]):
+            # Log automatically
             mlflow.keras.autolog()
+            
+            # Log the used config file
             mlflow.log_artifact(config_yaml_path)
 
+            # Create and train california dnn class
             california_dnn = CaliforniaDNN()
-
             california_dnn.build(**dnn_parameters)
             california_dnn.train(**dnn_train_parameters, **config_dataset)
 
+            # Log the accuracy
             mlflow.log_metric("test_loss", california_dnn.test_loss)
 
         return california_dnn
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Build and train california DNN")
+    parser = argparse.ArgumentParser(description="Train and evaluate california DNN")
 
-    # Add arguments: [https://qiita.com/kzkadc/items/e4fc7bc9c003de1eb6d0]
     parser.add_argument("-c", "--config_yaml_path", required=False, type=str, default="./config_california.yaml")
-
     args = parser.parse_args()
 
     CaliforniaDNN.run_all_process_with_mlflow(args.config_yaml_path)
